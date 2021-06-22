2f7ea74393c313de57e9001d1c13c4cdb357e237
knia10 <kelly.stephania@hotmail.com>
2021-06-22 02:20:55 +0000

    Add file 101 advanced

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - print all combinations of three different digits
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int oned;
+	int twod;
+	int threed;
+
+	for (threed = '0'; threed <= '9'; threed++)
+	{
+		for (two = (threed + 1); twod <= '9'; two++)
+		{
+			for (one = (twod + 1); oned <= '9'; oned++) /*ones*/
+			{
+				putchar(threed);
+				putchar(twod);
+				putchar(oned);
+				if (threed != '7' || twod != '8' || oned != '9')
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
