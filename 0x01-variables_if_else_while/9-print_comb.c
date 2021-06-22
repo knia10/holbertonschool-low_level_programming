2dab2588c8ded60e2b4b211b9fb075789fb97b33
knia10 <kelly.stephania@hotmail.com>
2021-06-22 02:01:52 +0000

    Add file 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *main - number separated with commas
+ *Description: use conditional
+ *Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int num = '0';
+
+	while (num <= '9')
+	{
+		putchar(n);
+		if (num != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}
