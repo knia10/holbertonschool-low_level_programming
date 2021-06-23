b9ce425a6912d71120b7cf3f2a037eedef2ea02b
knia10 <kelly.stephania@hotmail.com>
2021-06-23 03:41:37 +0000

    Add file 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 - function that prints all natural numbers from n to 98
+ * @n: number initial
+ */
+
+void print_to_98(int n)
+{
+	while (n < 98)
+	{
+		printf("%d, ", n);
+		n++;
+	}
+
+	while (n > 98)
+	{
+		printf("%d, ", n);
+		n--;
+	}
+
+	if (n == 98)
+		printf("%d", n);
+
+	printf("\n");
+}
--- 0x02-functions_nested_loops/holberton.h
+++ 0x02-functions_nested_loops/holberton.h
@@ -67,3 +67,9 @@ void times_table(void);
  */
 
 int add(int, int);
+
+/**
+ * print_to_98 -  function that prints all natural numbers from n to 98
+ */
+
+void print_to_98(int n);
