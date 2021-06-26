4cc354f713395066044d1c0b12b0fc44d182d559
knia10 <kelly.stephania@hotmail.com>
2021-06-26 02:48:17 +0000

    Add funtion 2larget

--- 0x03-debugging/holberton.h
+++ 0x03-debugging/holberton.h
@@ -6,3 +6,13 @@
 void positive_or_negative(int i);
 
 #endif /* HOLBERTON_H */
+
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
+
+int largest_number(int a, int b, int c);
