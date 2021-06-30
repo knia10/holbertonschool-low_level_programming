b1eb150c1e88373cbf4c1b623efdab1158c30042
knia10 <kelly.stephania@hotmail.com>
2021-06-30 02:40:27 +0000

    Add new Directory05

--- 0x04-more_functions_nested_loops/holberton.h
+++ 0x04-more_functions_nested_loops/holberton.h
@@ -1,6 +1,62 @@
 int _isupper(int c);
 /**
- *_isupperfunction that checks for uppercase character
+ *_isupper function that checks for uppercase character
  *@c: character to check
  *Returns: 1 if c is uppercase, 0 otherwise
 */
+
+int _isdigit(int c);
+/**
+ *_isdigit function that checks for a digit (0 through 9)
+ *@c: character to check
+ * Return: 1 if c a digit, 0 otherwise
+ */
+
+int mul(int a, int b);
+/**
+ * mul function that multiplies two integers.
+ *@a @b: character to multiplies
+ *Return: only multiplies
+ */
+
+void print_numbers(void)
+/**
+ *print_numbers function that prints the numbers, from 0 to 9
+ *Description: only use _putchar
+ *Return: numbers
+*/
+
+void print_most_numbers(void);
+/**
+ *print_most_numbers function that prints numbers minus 2 and 4
+ *Description: only use _putchar
+ *Return: numbers
+*/
+
+void more_numbers(void);
+/**
+ * more_numbers function that prints 10 times the numbers
+ *Description: from 0 to 14
+ *Return: numbers
+ */
+
+void print_line(int n);
+/**
+ *print_line function that draws a straight line 
+ *Description: where n is the number of times the characte
+ *Return: most line
+ */
+
+void print_diagonal(int n);
+/**
+ * print_diagonal unction that draws a diagonal line 
+ *Description: where n is the number of times the character \
+ *Return: new line
+ */
+
+void print_square(int size);
+/**
+ *print_square function that prints a square
+ *Description: where size is the size of the square
+ *Resturn:if size is 0 or less, the function should print only a new line
+ */
