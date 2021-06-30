a9820d8c56f666972a6fa0867e69290c85b2972c
knia10 <kelly.stephania@hotmail.com>
2021-06-30 03:35:15 +0000

    Add file 0 and file holberton

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ *reset_to_98 - function that changes the value of a variable with a pointer.
+ *Description: change variable.
+ *@n: variable to check
+ */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+return (n);
+}
---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,55 @@
+int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout
+ */
+
+void reset_to_98(int *n);
+/**
+ *reset_to_98 function that takes a pointer to an int as parameter and updates the value
+ */
+
+void swap_int(int *a, int *b);
+/**
+ *swap_int Write a function that swaps the values of two integers.
+ */
+
+int _strlen(char *s);
+/**
+ *_strlen Write a function that returns the length of a string.
+ */
+
+void _puts(char *str);
+/**
+ *_puts Write a function that prints a string, followed by a new line, to stdout.
+ */
+
+void print_rev(char *s);
+/**
+ *print_rev Write a function that prints a string, in reverse, followed by a new line.
+ */
+
+
+void rev_string(char *s);
+/**
+ *rev_string Write a function that reverses a string.
+ */
+
+void puts2(char *str);
+/**
+ *function that prints every other character of a string, starting with the first character,
+ */
+
+void puts_half(char *str);
+/**
+ *puts_half function that prints half of a string, followed by a new line.
+ */
+
+void print_array(int *a, int n);
+/**
+ *print_array function that prints n elements of an array of integers.
+ */
+
+char *_strcpy(char *dest, char *src);
+/**
+ *_strcpy function that copies the string pointed.
+ */
