c97a65ace056d38bdf5000c1cc60b0edbf8aa21e
knia10 <kelly.stephania@hotmail.com>
2021-06-30 03:45:54 +0000

    Add file 0 and file holberton

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -3,11 +3,13 @@
 /**
  *reset_to_98 - function that changes the value of a variable with a pointer.
  *Description: change variable.
- *@n: variable to check
+ *@n: pointer to check
  */
 
 void reset_to_98(int *n)
 {
 *n = 98;
-return (0);
+/**
+ *return (0);
+*/
 }
