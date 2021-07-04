b0f5de46c85e4c8f713a20dd9ebf8aad14a1867f
knia10 <kelly.stephania@hotmail.com>
2021-07-04 23:33:01 +0000

    Add file 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * *string_toupper - function that changes all lowercase letters to uppercase.
+ * @s: pointer chack.
+ *Return: string uppercase.
+  */
+
+char *string_toupper(char *s)
+{
+       	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 97 && s[i] <= 122)
+			s[i] = s[i] - 97 + 65;
+	}
+	return (s);
+}
