f3dfe2d6fee8b02caaf936b158917c20ce9467b6
knia10 <kelly.stephania@hotmail.com>
2021-07-04 17:54:09 +0000

    Add file putchar and holberton

---
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -0,0 +1,47 @@
+int _putchar(char c);
+
+/**
+ * _putchar - writes the character c to stdout
+ */
+
+char *_strcat(char *dest, char *src);
+
+/**
+ *_strcat - function that concatenates two strings.
+ */
+
+char *_strncat(char *dest, char *src, int n);
+
+/**
+ * _strncat -  function that concatenates two strings whit n src.
+ */
+
+char *_strncpy(char *dest, char *src, int n);
+/**
+ * _strncpy -  function that copies a string.
+ */
+
+int _strcmp(char *s1, char *s2);
+/**
+ *_strcmp - function that compares two strings.
+ */
+
+void reverse_array(int *a, int n);
+/**
+ * reverse_array - function that reverses the content of an array of integers.
+ */
+
+char *string_toupper(char *);
+/**
+ * string_toupper - function that changes all lowercase letters of a string to uppercase.
+ */
+
+char *cap_string(char *);
+/**
+ *cap_string - function that capitalizes all words of a string.
+ */
+
+char *leet(char *);
+/**
+ *leet - function that encodes a string into 1337.
+ */
