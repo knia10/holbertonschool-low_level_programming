20268383822b0555d650f9ba02e94be4dfeee9ed
knia10 <kelly.stephania@hotmail.com>
2021-07-07 20:45:41 +0000

    Add file putchar and header

---
+++ 0x08-recursion/holberton.h
@@ -0,0 +1,39 @@
+int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout.
+ */
+
+void _puts_recursion(char *s);
+/**
+ * _puts_recursion - that prints a string, followed by a new line.
+ */
+
+void _print_rev_recursion(char *s);
+/**
+ * _print_rev_recursion - function that prints a string in reverse.
+ */
+
+int _strlen_recursion(char *s);
+/**
+ *_strlen_recursion - Write a function that returns the length of a string.
+ */
+
+int factorial(int n);
+/**
+ *factorial - function that returns the factorial of a given number.
+ */
+
+int _pow_recursion(int x, int y);
+/**
+ *_pow_recursion - function that returns the value of x raised to the power of y.
+ */
+
+int _sqrt_recursion(int n);
+/**
+ *_sqrt_recursion - function that returns the natural square root of a number.
+ */
+
+int is_prime_number(int n);
+/**
+ *function that returns 1 if the input integer is a prime number, otherwise return 0.
+ */
