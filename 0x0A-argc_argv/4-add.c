db677405b6e4fa6bf23f6c4dc499232f0612c1c6
knia10 <kelly.stephania@hotmail.com>
2021-07-14 22:57:18 +0000

    Add file 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -3,42 +3,6 @@
 #include "holberton.h"
 
 /**
- * main - program that adds positive numbers.
- *@argc: counter arguments.
- *@argv: array of arguments.
- *Return: 1 if not is number.
- */
-
-int main(int argc, char *argv[])
-{
-	int i;
-	int add;
-
-	if (argc == 1)
-	{
-		printf("0\n");
-	}
-
-	add = 0;
-	
-	for (i = 1; argc > i; i++)
-	{
-		if (number(argv[i]))
-		{
-			add += atoi(argv[i]);
-		}
-		else
-		{
-			printf("error\n");
-			return (1);
-		}
-	}
-	printf("%i\n", add);
-	return (0);
-}
-
-
-/**
  * number - funtion verifique if number or other.
  *@arrg: array arguments.
  *Return: 1 if number, 0 is diferent to number.
@@ -56,4 +20,39 @@ bool number(char *arrg[])
 		}
 	}
 	return (1);
+}
+
+/**
+ * main - program that adds positive numbers.
+ *@argc: counter arguments.
+ *@argv: array of arguments.
+ *Return: 1 if not is number.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+	int add;
+
+	if (argc == 1)
+	{
+		printf("0\n");
+	}
+
+	add = 0;
+
+	for (i = 1; argc > i; i++)
+	{
+		if (number(argv[i]))
+		{
+			add += atoi(argv[i]);
+		}
+		else
+		{
+			printf("error\n");
+			return (1);
+		}
+	}
+	printf("%i\n", add);
+	return (0);
 }
