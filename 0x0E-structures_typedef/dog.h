d2da35f2650ee54651884cd918538aa36e24e803
knia10 <kelly.stephania@hotmail.com>
2021-07-21 16:48:22 +0000

    Add typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,20 +2,27 @@
 #define _DOG_H_
 
 /**
- * struct dog -  new type struct.
+ * dog_t - new type struct.
+ */
+
+typedef struct dog dog_t;
+
+
+
+/**
+ * struct dog - origin struct.
  *@name: name of dog.
  *@age: age of dog.
  *@owner: owner of dog.
  */
 
-
 struct dog
+
 {
 	char *name;
 	float age;
 	char *owner;
 };
-
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
