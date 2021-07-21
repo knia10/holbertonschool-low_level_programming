d3ccd11f779f6dda58bf48544bc3bedb8c68d788
knia10 <kelly.stephania@hotmail.com>
2021-07-21 23:05:38 +0000

    Add header and putchar

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,25 @@
+#ifndef _FUNCTION_POINTERS_H_
+#define _FUNCTION_POINTERS_H_
+
+int _putchar(char c);
+/**
+ * _putchar - writes the character c to stdout
+ */
+
+void print_name(char *name, void (*f)(char *));
+/**
+ * function that prints a name.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int));
+/**
+ *function that executes a function given as a parameter on each element of an array.
+ */
+
+int int_index(int *array, int size, int (*cmp)(int));
+/**
+ * function that searches for an integer.
+ */
+
+
+#endif
