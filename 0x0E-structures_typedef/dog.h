8a875ee316a9c3742cbcf04c47b2f6eb720f3080
knia10 <kelly.stephania@hotmail.com>
2021-07-21 18:38:56 +0000

    Add file 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,10 @@
 #define _DOG_H_
 
 /**
- * dog_t - new type struct.
+ * dog_t - new type for struct dog.
  */
 
 typedef struct dog dog_t;
-
-
 
 /**
  * struct dog - origin struct.
@@ -26,5 +24,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
 
 #endif
