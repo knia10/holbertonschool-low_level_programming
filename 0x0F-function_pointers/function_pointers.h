79a1e4dc6c8426d1eb150a83f76a57b4be13f3a4
knia10 <kelly.stephania@hotmail.com>
2021-07-22 01:07:11 +0000

    Add file 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,26 +1,15 @@
 #ifndef POINTERS
 #define POINTERS
 
+#include <stddef.h>
 
 int _putchar(char c);
-/**
- * _putchar - writes the character c to stdout
- */
 
 void print_name(char *name, void (*f)(char *));
-/**
- * function that prints a name.
- */
 
 void array_iterator(int *array, size_t size, void (*action)(int));
-/**
- *function that executes a function given as a parameter on each element of an array.
- */
 
 int int_index(int *array, int size, int (*cmp)(int));
-/**
- * function that searches for an integer.
- */
 
 
 #endif
