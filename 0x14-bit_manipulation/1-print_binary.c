c800e87587de8e35ce6802b720fb8b6f7a8c9253
knia10 <kelly.stephania@hotmail.com>
2021-08-05 22:13:35 -0500

    add file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,9 @@
 #include "main.h"
+
 /**
- * print_binary - prints the binary representation of a number.
- * @n: unsigned long int.
- */
+* print_binary - Convert function to unsigned int
+* @n: this is pointer
+*/
 
 void print_binary(unsigned long int n)
 {
